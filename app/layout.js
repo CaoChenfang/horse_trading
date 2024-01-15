45645589b7e4bc1ad144d6040d542f707a63639f
hai12strong <115347394+hai12strong@users.noreply.github.com>
2024-01-15 17:10:35 +0000

    addAnalytics

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { AuthProvider } from "./Providers";
 import "./globals.css";
 import { Inter } from "next/font/google";
-
+import { Analytics } from '@vercel/analytics/react';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -14,6 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <AuthProvider>{children}</AuthProvider>
+        <Analytics />
       </body>
     </html>
   );
